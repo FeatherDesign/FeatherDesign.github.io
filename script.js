8817aae9057952305c006f2958f5cf028ba6a452
FeatherDesign <bogdan.ponomarenko.777@gmail.com>
2019-01-10 15:06:40 +0200

    1.5

--- script.js
+++ script.js
@@ -1,48 +1,96 @@
-// $(function() {
-//     var btnF = $('span.F');
-//     function editWeight(t,s,r,i){
-//         for(1==t?s.css({"font-weight":300}):0==t&&s.css({"font-weight":r}),n=0;n<i;)
-//             setTimeout(function(){w=s.css("font-weight"),
-//                 1==t?w=Number(w)+100:0==t&&(w=Number(w)-100),
-//                 s.css({"font-weight":w})},30*n),n++
-//     };
-//     btnF.click(function() {
-//         // $('span.F').css({'position': 'absolute'});
-//         if ($('span.e').css('font-weight') == '300') {
-//             editWeight(true, $('span.e'), 800, 5);
-//         }
-//         else if ($('span.e').css('font-weight') == '800') {
-//             editWeight(false, $('span.e'), 800, 5);
-//         } 
-//         if ($('span.a').css('font-weight') == '300') {
-//             editWeight(true, $('span.a'), 700, 4);
-//         }
-//         else if ($('span.a').css('font-weight') == '700') {
-//             editWeight(false, $('span.a'), 700, 4);
-//         } 
-//         if ($('span.t').css('font-weight') == '300') {
-//             editWeight(true, $('span.t'), 600, 3);
-//         }
-//         else if ($('span.t').css('font-weight') == '600') {
-//             editWeight(false, $('span.t'), 600, 3);
-//         } 
-//         if ($('span.h').css('font-weight') == '300') {
-//             editWeight(true, $('span.h'), 500, 2);
-//         }
-//         else if ($('span.h').css('font-weight') == '500') {
-//             editWeight(false, $('span.h'), 500, 2);
-//         } 
-//         if ($('span.e2').css('font-weight') == '300') {
-//             editWeight(true, $('span.e2'), 400, 1);
-//         }
-//         else if ($('span.e2').css('font-weight') == '400') {
-//             editWeight(false, $('span.e2'), 400, 1);
-//         } 
-//     });
-//     // $('.other span').hover(function(){
-//     //     editWeight(true, $(this), 800, 5);
-//     // });
-// });
+class TextScramble {
+  constructor(el) {
+    this.el = el
+    this.chars = '>-_\\]{}—=+*^?#________'
+    this.update = this.update.bind(this)
+  }
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 40)
+      const end = start + Math.floor(Math.random() * 40)
+      this.queue.push({ from, to, start, end })
+    }
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
+        }
+        output += `<span class="dud">${char}</span>`
+      } else {
+        output += from
+      }
+    }
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
+  }
+}
+
+$('span.F').click(function(){
+		$('.infoText').toggleClass('infoText_animation')
+		setTimeout(function() {
+			$('.infoText').removeClass('infoText_animation')
+		}, 700)
+		
+});
+
+var timeout = 1
+$('span.F').click(function(){
+	
+	if (timeout == 1) {
+		timeout = 0;
+	} else if (timeout == 0) {
+		timeout = 1;
+	}
+	console.log(timeout);
+});
+
+const phrases = [
+  'Me..',
+  'This is something like my personal blog.',
+  'I don\'t know why I did it..',
+  'but..',
+  'I hope you\'ll like it   :^)'
+]
+
+const el = document.querySelector('.text')
+const fx = new TextScramble(el)
+
+let counter = 0
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 2500)
+  })
+  counter = (counter + timeout) % phrases.length
+}
+
+next()
 
 
 
